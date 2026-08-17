653a1a7ffea7e88ccc3aa35df641bf73079c27b3
Yevheniia <sopli15011970@gmail.com>
2026-08-17 18:29:46 +0200

    new file

---
+++ format_str.py
@@ -0,0 +1,27 @@
+# name = 'Alisa'
+# age = 30
+
+
+#Пурший с з змінної message
+# message = f'My name is {name}. I am {age/2} years old' 
+# print(message)
+
+#Пурший варіант форматування з змінної print
+# print(f'My name is {name}. I am {age/2} years old')
+
+
+#Другий  варіант форматування з змінної message
+# message = 'My name is {}. I am {} years old'.format(name, age) 
+# print(message) 
+
+#Третій  варіант форматування
+# message = 'My name is %s. I am %d years old' % (name, age) # S-для строки  d-для цифер 
+# print(message)
+
+
+# message = 'My name is ' + name + ' . I am  ' + str(age) + ' years old' 
+# print(message)
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,19 @@
+# name = 'John' #str
+# age = 25 #int
+# height = 1.67 #float
+# is_student = True #False #Boolean
+# has_high_degree = None #None
+
+# print(name)
+# print(age)
+# print(height)
+# print(is_student)
+# print(has_high_degree)
+
+
+# name, age = 'John', 25 #Множинне присвоєвання але краще не використовувати всюди.
+
+# print(name)
+# print(age)
+
+
